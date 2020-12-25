c9e32cce0f687fd91cd07df3ca0ebc7e559673ef
vakkaya1 <vakkaya1@yahoo.com>
2020-12-25 01:36:29 +0100

    5. commit

--- src/test/java/Runners/_US_00_RegressionTest.java
+++ src/test/java/Runners/_US_00_RegressionTest.java
@@ -13,11 +13,11 @@ import org.testng.annotations.Parameters;
        tags = {"@RegressionTest"},
         features = {"src\\test\\java\\FeatureFiles"},
         glue = {"StepDefinitions"},
-        dryRun = false,
-        plugin = {
-                "com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport/ExtentReportRegression.html"
+        dryRun = false
+     //   plugin = {
+               // "com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport/ExtentReportRegression.html"
                 // oluşturalacak raporun yeri ve adı veriliyor.
-        }
+      //  }
 )
 public class _US_00_RegressionTest extends AbstractTestNGCucumberTests {
 
